d2de6ceec6f2e05d261ebeddc1c9150b99a59313
Lobsiinvok <qazbnm456@gmail.com>
2016-12-12 16:01:27 +0800

    add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup, find_packages
+
+ROOT_DIR = os.path.dirname(__file__)
+SOURCE_DIR = os.path.join(ROOT_DIR)
+
+requirements = [
+    'docker-py >= 1.10.6',
+    'dockerpty >= 0.4.1',
+    'arrow >= 0.10.0',
+    'humanize >= 0.5.1',
+    'pystache >= 0.5.4',
+    'colorama >= 0.3.7'
+]
+
+version = None
+exec(open('version.py').read())
+
+setup(name='dokcer',
+      version=version,
+      description='Python based Assistance for Docker',
+      author='Boik Su',
+      author_email='boik@tdohacker.org',
+      url='https://github.com/qazbnm456/dokcer',
+      packages=find_packages(),
+      install_requires=requirements
+)
